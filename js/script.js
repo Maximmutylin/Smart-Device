b81a7c91960a61cbf356f2c196c2c613e007313c
Maximmutylin <maksimmutylin@gmail.com>
2022-05-29 00:18:12 +0300

    add ankors and flowing scroll

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     const textChange = () => {
         if (window.innerWidth < 750) {
             $('.header__button').text('Бесплатная консультация');
+            $('.header__button').css('padding-top', '20px')
         } else {
             $('.header__button').text('Получить бесплатную консультацию');
         }
@@ -15,7 +16,15 @@ $(document).ready(function () {
     textChange();
     window.addEventListener('resize', textChange);
 
-    
+    $('a.link-btn').click(function() {
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top + "px"
+         }, {
+            duration: 1000,
+            easing: "swing"
+         });
+         return false;
+    })
     
 })
 
